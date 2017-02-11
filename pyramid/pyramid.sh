3df344f87e23190272addf449917ac2333d795be
jstevens00 <joeystevens00@gmail.com>
2017-02-11 14:54:08 -0500

    added arg parsing, hourglass printing, and reverse pyramid printing

--- pyramid/pyramid.sh
+++ pyramid/pyramid.sh
@@ -20,17 +20,67 @@ function toCenter() {
 
 
 function makePyramid() {
-	for i in $(seq 1 $1); do                                                                                          
+	count=$(seq 1 $1) 
+	if [ "$reversePyramid" ]; then count=$(echo -e "$count" | sort --numeric-sort --reverse); fi
+	for i in $(echo -e "$count"); do  
 		x=$(randomChar "$i")
 		y=$(randomChar "$i")
 		z=$(randomChar "$i")
 		toCenter "$x$y$z"
 	done
 }
+ 
+function displayHelp() {
+	cat << helpcontent
+$0 --size=[option] 
+	-r |--reverse	prints an upside down pyramid
+	-s |--size		sets the height of the pyramid (default is terminal height)
+	-g | --hourglass prints an hourglass
+	-h | --help     displays this page
+helpcontent
+exit 1
+}
 
-pyramidHeight="$1"
-if [ -z "$pyramidHeight" ]; then
+function argParse() {
+	for i in "$@"; do
+	case $i in
+		-r|--reverse)
+			reversePyramid=1
+             ;;
+		-g|--hourglass)
+    		hourglass=1
+    		;;
+		-s=*|--size=*)
+    		pyramidHeight="${i#*=}"
+    		shift # past argument=value
+    		;;
+        -h|--help=*)
+    		help=true
+            shift # past argument=value
+        	;;
+    	*)
+	       	help=true # unknown option
+    		;;
+	esac
+	done
 	term_height=$(tput lines)
-	pyramidHeight=$((term_height-2))
-fi
-makePyramid "$pyramidHeight"
+	if [ "$help" == true ]; then displayHelp; fi
+	if [ -z "$pyramidHeight" ] && [ -z "$hourglass" ]; then
+		pyramidHeight=$((term_height-2))
+	elif [ "$hourglass" ] && [ -z "$pyramidHeight" ]; then
+		pyramidHeight=$((term_height/2-1))
+	fi
+
+	if [ "$hourglass" ]; then 
+		reversePyramid=1
+		makePyramid "$pyramidHeight"
+		unset reversePyramid
+		makePyramid "$pyramidHeight"
+	else 
+		makePyramid "$pyramidHeight"
+	fi
+}
+
+
+argParse "$@"
+
