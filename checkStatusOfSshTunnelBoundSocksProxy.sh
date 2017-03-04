f6c23048a7ef1172ca2b191615764336303d650a
jstevens00 <joeystevens00@gmail.com>
2017-03-04 14:24:51 -0500

    ssh randomly decided to use ipv6. -4 flag will prevent this from happening in the future debug1: Local forwarding listening on ::1 port 9999.

--- checkStatusOfSshTunnelBoundSocksProxy.sh
+++ checkStatusOfSshTunnelBoundSocksProxy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Checks that an SSH tunnel that is bound to a local port (effectively making a SOCKS proxy) is still up
-# If it finds the tunnel is dead it will kill the ssh processes and restart the tunnel
-# Best useds as a cronjob ran every few minutes  (5 seems OK)
-# */5 * * * * /root/bin/checkStatusOfSshTunnelBoundSocksProxy.sh > /root/checkStatus.log 2> /root/checkStatuserror.log
-
 sshproxybindhost="localhost"
 sshproxybindport="9999"
 sshproxy=""
@@ -11,8 +6,13 @@ sshproxyuser=""
 password=""
 
 proxySet() {
-sshpass -p $1 ssh -N -D $2 $3 $4 $5 &
+	password="$1"
+	user="$2"
+	server="$3"
+	port="$4"
+	sshpass -p "$password" ssh -N -4 -v "$user"@"$server" -D "$port" &
 }
+
 checkStatus() {
 	proxyStatus=`curl --connect-timeout 5 -Is --socks5 $sshproxybindhost:$sshproxybindport google.com`
 }
@@ -24,10 +24,11 @@ else
 	retry=0
 	until [ "$proxyStatus" ]  || ((retry>=3)); do
 		echo "$proxyStatus"
-		currentServicePid=` ps aux | grep ssh | grep sshpass | grep "$sshproxybindport" | grep "$sshproxy" | awk {'print $2'}`
+		currentServicePid=` ps aux | grep ssh | grep "$sshproxybindport" | grep "$sshproxy" | awk {'print $2'}`
 		kill -9 $currentServicePid
+		sleep 1
 		proxySet $password $sshproxybindport $sshproxyuser@$sshproxy
-		sleep 20
+		sleep 19
 		checkStatus
 		((retry++))
 	done
