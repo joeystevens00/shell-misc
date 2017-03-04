2a2f19e3258e27d1db958ef4c594971a2e93d61b
jstevens00 <joeystevens00@gmail.com>
2017-03-04 15:26:39 -0500

    seems like curl doesn't alawys work. do the connection over NC first and if that fails check curl.. If that fails its probably safe to assume something went wrong

--- checkStatusOfSshTunnelBoundSocksProxy.sh
+++ checkStatusOfSshTunnelBoundSocksProxy.sh
@@ -14,26 +14,38 @@ proxySet() {
 	user="$2"
 	server="$3"
 	port="$4"
-	sshpass -p "$password" ssh -N -4 -v "$user"@"$server" -D "$port" &
+	sshpass -p "$password" ssh -N -4 "$user"@"$server" -D "$port" &
 }
 
 checkStatus() {
-	proxyStatus=`curl --connect-timeout 5 -Is --socks5 $sshproxybindhost:$sshproxybindport google.com`
+	# Returns 0 if success
+	# Returns 1 if failure
+	tryNc=`printf "HEAD / HTTP/1.0\r\n\r\n" | nc -X 5 -x $sshproxybindhost:$sshproxybindport google.com 80`
+	if [ "$tryNc" ]; then
+		return 0
+	else 
+		tryCurl=`curl --connect-timeout 5 -Is --socks5 $sshproxybindhost:$sshproxybindport google.com`
+		if [ "$tryCurl" ]; then
+			return 0
+		else 
+			return 1
+		fi
+	return 1
+	fi
 }
 
-checkStatus
-if [ "$proxyStatus" ]; then 
+checkStatus ; proxyStatus=$?
+if (($proxyStatus == 0)); then 
 	exit 0
 else
 	retry=0
-	until [ "$proxyStatus" ]  || ((retry>=3)); do
-		echo "$proxyStatus"
+	until (( "$proxyStatus" == 0 ))  || ((retry>=3)); do
 		currentServicePid=` ps aux | grep ssh | grep "$sshproxybindport" | grep "$sshproxy" | awk {'print $2'}`
 		kill -9 $currentServicePid
-		sleep 1
-		proxySet $password $sshproxybindport $sshproxyuser@$sshproxy
-		sleep 19
-		checkStatus
+		for pid in `echo -e "$currentServicePid"`; do kill -9 $pid; done
+		proxySet $password $sshproxyuser $sshproxy $sshproxybindport 
+		sleep 15
+		checkStatus ; proxyStatus=$?
 		((retry++))
 	done
 fi
